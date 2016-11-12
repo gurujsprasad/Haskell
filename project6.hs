685b18c2da57b76c8783d80541abef38e9743814
Guruprasad <gurujsprasad@gmail.com>
2016-11-12 13:29:38 -0800

    project 6 - Largest number possible from a given integer list

--- project6.hs
+++ project6.hs
@@ -1,20 +1,14 @@
 import Data.List
 import Data.Ord
 
-
 concatNum :: Int -> Int -> Int
 concatNum x y = read ((show x) ++ (show y))
 
 myCompare :: Int -> Int -> Ordering
 myCompare x y = flip compare (concatNum x y) (concatNum y x)
 
-
-mydot ::  (b -> c) -> (a -> b) -> (a -> c)
+mydot ::  (b -> c) -> (a -> b) -> a -> c
 mydot f g = \x -> f (g x)
 
-
-myflip ::  (a -> b -> c) -> (b -> a -> c)
-myflip f x y= f y x
-
 largestNumber :: [Int] -> Integer
-largestNumber =  read . concat . map show . (sortBy myCompare)
+largestNumber =  read `mydot` (concat `mydot` (map show `mydot` sortBy myCompare))
